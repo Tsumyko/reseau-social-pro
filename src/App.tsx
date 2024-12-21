ba6900021e894bbe78ef122c36dc7fd233d2a55d
Tsumyko <77759900+Tsumyko@users.noreply.github.com>
2024-12-21 09:28:26 +0100

    Mise à jour des routes avec authentification

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Layout from './components/layout/Layout';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Login from './pages/auth/Login';
+import Signup from './pages/auth/Signup';
+import { RootState } from './store';
 
-function App() {
-  return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<div>Home Page</div>} />
-          <Route path="/profile" element={<div>Profile Page</div>} />
-          <Route path="/news" element={<div>News Page</div>} />
-        </Routes>
-      </Layout>
-    </Router>
-  );
+function PrivateRoute({ children }: { children: React.ReactNode }) {
+  const isAuthenticated = useSelector((state: RootState) => !!state.auth.token);
+  return isAuthenticated ? <>{children}</> : <Navigate to="/login" />;
 }
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={
+          <PrivateRoute>
+            <Layout>
+              <Home />
+            </Layout>
+          </PrivateRoute>
+        } />
+        <Route path="/profile" element={
+          <PrivateRoute>
+            <Layout>
+              <Profile />
+            </Layout>
+          </PrivateRoute>
+        } />
+      </Routes>
+    </BrowserRouter>
+  );
+}
